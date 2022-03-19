3d1e17f61aff4b9b18abea8bdf04ba8c4a3ab9ab
astolfovalentim <astolfom@gmail.com>
2022-03-19 16:49:05 -0300

    ok

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+const botao = document.querySelector("button");
+
+const imagem = document.querySelector("img");
+
+const textoFoto = document.querySelector("h2");
+
+botao.addEventListener("click", () => {
+  if (botao.value == 1) {
+    imagem.src = "./img/hulk.jpg";
+    textoFoto.innerHTML = "Bruce ficou com raiva e virou o Hulk";
+    botao.setAttribute("value", 2);
+  } else if (botao.value == 2) {
+    imagem.src = "./img/hulkEsmaga.jpg";
+    textoFoto.innerHTML = "HULK ESMAGA!!!";
+    botao.setAttribute("value", 3);
+  } else if (botao.value == 3) {
+    imagem.src = "./img/hulkfeliz.jpg";
+    textoFoto.innerHTML = "Hulk está feliz!";
+    botao.setAttribute("value", 4);
+  } else {
+    imagem.src = "./img/bruce.jpg";
+    textoFoto.innerHTML = "Bruce Banner está de boa";
+    botao.setAttribute("value", 1);
+  }
+});
